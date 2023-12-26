ecbc659c33da1318cc4dcc4d02ed718e9af424dc
Steve Green <home@steve-green.com>
2023-12-26 11:39:16 +0000

    Initial Commit - Tutorial completed

---
+++ src/error-page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage: React.FC = () => {
+    const error:unknown= useRouteError();
+    console.error(error);
+
+    return (
+        <div id="error-page">
+            <h1>Boom!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+            <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+    ); 
+}
+
+export default ErrorPage;
